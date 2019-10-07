ae99b0f477b59022342509a6bf5afa36eb984fb5
summerlin <summerchihuilin@gmail.com>
2019-10-07 16:50:15 +0800

    Q2 so far so good

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,15 +10,28 @@ class WelcomeController < ApplicationController
 
   def max_foods
     #Q2: It should return a maximum number of food across all the meals.
-    @meals = Meals.all
-    @meal.food.count
+    a = [ "coke", "burger", "apple_pie" ]
+    b = [ ]
+    c = [ "coke", "french_fries", "fried_chicken" ]
+    d = [ "apple_pie" ]
+    e = [ ]
+
+    @coke = a.join(" ").count"coke" + b.join(" ").count"coke" + c.join(" ").count"coke" + d.join(" ").count"coke" + e.join(" ").count"coke"
+    @burger = a.join(" ").count"burger" + b.join(" ").count"burger" + c.join(" ").count"burger" + d.join(" ").count"burger" + e.join(" ").count"burger"
+    @apple_pie = a.join(" ").count"apple_pie" + b.join(" ").count"apple_pie" + c.join(" ").count"apple_pie" + d.join(" ").count"apple_pie" + e.join(" ").count"apple_pie"
+    @french_fries = a.join(" ").count"apple_pie" + b.join(" ").count"apple_pie" + c.join(" ").count"apple_pie" + d.join(" ").count"apple_pie" + e.join(" ").count"apple_pie"
+    @fried_chicken = a.join(" ").count"fried_chicken" + b.join(" ").count"fried_chicken" + c.join(" ").count"fried_chicken" + d.join(" ").count"fried_chicken" + e.join(" ").count"fried_chicken"
 
   end
 
   def other_food
     #Q3: It should return a list of every food as Food Head; with each Food Head,
-    #    list the pairing food from all the meal combination.
-
+        #list the pairing food from all the meal combination.
+    @apple_pie
+    @burger
+    @coke
+    @french_fries
+    @fried_chicken
   end
 
 end
